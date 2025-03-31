7bde1b26ae3a59139f6c9255f514214e2fb4be8f
CamiloAbarca <camilo.abarca.21@gmail.com>
2025-03-31 10:41:10 -0300

    Creación NotFound

--- src/router/index.js
+++ src/router/index.js
@@ -3,6 +3,7 @@ import VueRouter from 'vue-router'
 import DashboardView from '../views/DashboardView.vue'
 import EquiposView from '@/views/EquiposView.vue'
 import ClientesView from '@/views/ClientesView.vue'
+import NotFoundView from '@/views/NotFoundView.vue'
 
 Vue.use(VueRouter)
 
@@ -24,6 +25,12 @@ const routes = [
     name: 'clientes',
     component: ClientesView
   },
+
+  {
+    path: '*',
+    name: 'notfound',
+    component: NotFoundView
+  }
 ]
 
 const router = new VueRouter({
---
+++ src/views/NotFoundView.vue
@@ -0,0 +1,22 @@
+<template>
+  <div>
+    <img :src="NotFound" alt="NotFound" height="500px">
+  </div>
+</template>
+
+<script>
+import NotFound from '@/assets/NotFound.png'
+export default {
+  name: 'NotFoundView',
+  data() {
+        return {
+            NotFound
+        };
+    }
+
+}
+</script>
+
+<style>
+
+</style>
